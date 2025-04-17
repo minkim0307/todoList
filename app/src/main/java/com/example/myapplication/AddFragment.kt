cf3e5f96a37fd4433dfa01c59e4631a4d493974f
Dashboard <zxzx1332@naver.com>
2025-04-17 14:54:43 +0900

    no message

--- app/src/main/java/com/example/myapplication/AddFragment.kt
+++ app/src/main/java/com/example/myapplication/AddFragment.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.myapplication.databinding.FragmentAddBinding
